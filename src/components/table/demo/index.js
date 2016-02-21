504f32268c80d14fb8fc7d2b653222f517a46b22
Steen Molberg <stm@globeteam.com>
2016-02-21 11:40:42 +0100

    Selection troubleshooting

--- src/components/table/demo/index.js
+++ src/components/table/demo/index.js
@@ -35,4 +35,47 @@ function demoController($scope) {
             isSelected: false
         }
     ];
+
+
+            $scope.files2 = [
+        {
+            fileName: 'file1.txt',
+            location: '~/file1.txt',
+            modified: new Date(),
+            type: 'TXT',
+            size: 20,
+            isSelected: false
+        },
+        {
+            fileName: 'file2.docx',
+            location: '~/file2.docx',
+            modified: new Date(),
+            type: 'DOCX',
+            size: 5,
+            isSelected: true
+        },
+        {
+            fileName: 'file3.txt',
+            location: '~/file3.txt',
+            modified: new Date(),
+            type: 'TXT',
+            size: 10,
+            isSelected: false
+        }
+    ];
+
+
+    $scope.showSelected = function(){
+
+      console.log($scope.table.selectedItems);
+
+    }
+
+    $scope.showFiles = function(){
+
+      console.log($scope.files);
+
+    }
+
+
 }
